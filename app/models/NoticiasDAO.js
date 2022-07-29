27f10e2bdcf208f646a7ce88c9e0804ad8c66fc0
ricardolinsnascimento <ricardo.qlikview@gmail.com>
2022-07-29 15:39:52 -0400

    quarto

--- app/models/NoticiasDAO.js
+++ app/models/NoticiasDAO.js
@@ -1,39 +1,38 @@
 function NoticiasDAO(connection){
-	this._connection = connection;
+	conn = connection;
+
+	this.getNoticias = function(callback){
+		conn.query('select * from noticias ORDER BY data_criacao DESC', callback);
+	}
+
+	this.getNoticia = function(id_noticia, callback){
+		conn.query('select * from noticias where id_noticia = ' + id_noticia.id_noticia, callback);
+    }
+
+	this.salvarNoticia = function(noticia, callback){
+		conn.query('insert into noticias set ?', noticia, callback);
+	}
+
+	this.get5UltimasNoticias = function(callback){
+		conn.query('select * from noticias order by data_criacao desc limit 5', callback);
+    }
+
+    this.buscaNoticias = function(pesquisa, callback){
+		conn.query('select * from noticias where titulo like ?', '%' + pesquisa + '%', callback);
+    }
+
+    this.excluiNoticia = function(id_noticia, callback){
+		conn.query('delete from noticias where id_noticia = '+ id_noticia.id_noticia, callback);
+    }
+
+    this.atualizarNoticia = function(noticia, callback){
+		conn.query("update noticias set titulo = '" + noticia.titulo +"', resumo = '" + noticia.resumo + "', autor = '" + noticia.autor + "', noticia = '" + noticia.noticia + "'where id_noticia = " + noticia.id_noticia, callback);
+    }
+
+    this.mostraNoticia = function(id_noticia, callback){
+		conn.query('select * from noticias where id_noticia = ' + id_noticia, callback);
+    }
 }
-	NoticiasDAO.prototype.getNoticias = function(callback){
-		this._connection.query('select * from noticias ORDER BY data_criacao DESC', callback);
-	}
-
-
-	NoticiasDAO.prototype.getNoticia = function(id_noticia, callback){
-	this._connection.query('select * from noticias where id_noticia = ' + id_noticia.id_noticia, callback);
-    }
-
-	NoticiasDAO.prototype.salvarNoticia = function(noticia, callback){
-		this._connection.query('insert into noticias set ?', noticia, callback);
-	}
-
-	NoticiasDAO.prototype.get5UltimasNoticias = function(callback){
-	this._connection.query('select * from noticias order by data_criacao desc limit 5', callback);
-    }
-
-    NoticiasDAO.prototype.buscaNoticias = function(pesquisa, callback){
-	this._connection.query('select * from noticias where titulo like ?', '%' + pesquisa + '%', callback);
-    }
-
-    NoticiasDAO.prototype.excluiNoticia = function(id_noticia, callback){
-	this._connection.query('delete from noticias where id_noticia = '+ id_noticia.id_noticia, callback);
-    }
-
-    NoticiasDAO.prototype.atualizarNoticia = function(noticia, callback){
-	this._connection.query("update noticias set titulo = '" + noticia.titulo +"', resumo = '" + noticia.resumo + "', autor = '" + noticia.autor + "', noticia = '" + noticia.noticia + "'where id_noticia = " + noticia.id_noticia, callback);
-    }
-
-    NoticiasDAO.prototype.mostraNoticia = function(id_noticia, callback){
-	this._connection.query('select * from noticias where id_noticia = ' + id_noticia, callback);
-    }
     
-module.exports = function(){
-return NoticiasDAO;
-}
+module.exports = NoticiasDAO;
+
